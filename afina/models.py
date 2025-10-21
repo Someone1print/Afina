9cb60baa1005f73ccb1901f04de0f4ce962729d5
Mika <kamila44043@gmail.com>
2025-10-21 11:06:24 +0600

    Add ProfileModule

--- afina/models.py
+++ afina/models.py
@@ -8,3 +8,18 @@ class IncomeCategory(models.Model):
 
     class Meta:
         db_table = 'income_category'  # имя твоей существующей таблицы
+
+    class Profile(models.Model):
+        user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+        full_name = models.CharField("ФИО", max_length=150, blank=True)
+        currency = models.CharField("Валюта по умолчанию", max_length=10, default="KGS")
+
+        # avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)  # если нужно
+
+        class Meta:
+            db_table = 'profile'
+            verbose_name = "Профиль"
+            verbose_name_plural = "Профили"
+
+        def _str_(self):
+            return self.full_name or self.user.username
