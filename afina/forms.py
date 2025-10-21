14beffcd6b0d1f3a482cefee19fbb6a65d05684f
Ilias <you@example.com>
2025-10-21 12:02:14 +0600

    added income model

--- afina/forms.py
+++ afina/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import IncomeCategory
+from .models import IncomeCategory, ExpenseCategory, Income, Expense, Profile
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -16,3 +16,24 @@ class IncomeCategoryForm(forms.ModelForm):
     class Meta:
         model = IncomeCategory
         fields = ['incomeName']
+
+class ExpenseCategoryForm(forms.ModelForm):
+    class Meta:
+        model = ExpenseCategory
+        fields = ["expenseName"]
+
+class IncomeForm(forms.ModelForm):
+    class Meta:
+        model = Income
+        fields = ["category", "date", "amount", "note"]
+
+class ExpenseForm(forms.ModelForm):
+    class Meta:
+        model = Expense
+        fields = ["category", "date", "amount", "note"]
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["full_name", "currency"]
+        # fields = ["full_name", "currency", "avatar"]
