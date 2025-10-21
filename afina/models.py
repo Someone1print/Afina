9a8c3f00be6cebbe945e8fd288144c5bdaf8066f
Your Name <you@example.com>
2025-10-21 11:02:38 +0600

    added income model

--- afina/models.py
+++ afina/models.py
@@ -8,3 +8,20 @@ class IncomeCategory(models.Model):
 
     class Meta:
         db_table = 'income_category'  # имя твоей существующей таблицы
+
+class Income(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='incomes')
+    category = models.ForeignKey(IncomeCategory, on_delete=models.PROTECT, related_name='incomes')
+    date = models.DateField()
+    amount = models.DecimalField(max_digits=14, decimal_places=2)
+    note = models.CharField(max_length=255, blank=True)
+
+    class Meta:
+        db_table = 'income'
+        ordering = ['-date', '-id']
+        verbose_name = "Доход"
+        verbose_name_plural = "Доходы"
+
+    def _str_(self):
+        return f"{self.date}: {self.amount} ({self.category})"
+
