81086f35229a3f59721ca0236e74d21dc51feee1
Mika <kamila44043@gmail.com>
2025-10-22 21:17:17 +0600

    Little error

--- afina/models.py
+++ afina/models.py
@@ -10,20 +10,6 @@ class IncomeCategory(models.Model):
         db_table = 'income_category'  # имя твоей существующей таблицы
 
 
-    class Profile(models.Model):
-        user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-        full_name = models.CharField("ФИО", max_length=150, blank=True)
-        currency = models.CharField("Валюта по умолчанию", max_length=10, default="KGS")
-
-        # avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)  # если нужно
-
-        class Meta:
-            db_table = 'profile'
-            verbose_name = "Профиль"
-            verbose_name_plural = "Профили"
-
-        def _str_(self):
-            return self.full_name or self.user.username
 class ExpenseCategory(models.Model):
     id = models.AutoField(primary_key=True)
     expenseName = models.CharField(max_length=100, unique=True)
