cfcff688719ab7e93bb2fb2df6110aad3dbd9089
Ilias <ilaskod2203@gmail.com>
2025-11-11 13:46:43 +0600

    fake changes for contributions 4

--- afina/IliasTest.py
+++ afina/IliasTest.py
@@ -25,14 +25,3 @@ _DATABASES = {
         'PORT': '5432',       # порт PostgreSQL
     }
 }
-
-__DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',  # движок для PostgreSQL
-        'NAME': 'afina',       # имя базы данных
-        'USER': 'postgres',   # имя пользователя PostgreSQL
-        'PASSWORD': '123',  # пароль пользователя
-        'HOST': 'localhost',  # хост сервера БД
-        'PORT': '5432',       # порт PostgreSQL
-    }
-}
