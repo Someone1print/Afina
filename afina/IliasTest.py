4db4c01eb7890adaa1932b49f4e301a2f0325ce0
Ilias <ilaskod2203@gmail.com>
2025-11-11 13:45:52 +0600

    fake changes for contributions

---
+++ afina/IliasTest.py
@@ -0,0 +1,60 @@
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',  # движок для PostgreSQL
+        'NAME': 'afina',       # имя базы данных
+        'USER': 'postgres',   # имя пользователя PostgreSQL
+        'PASSWORD': '123',  # пароль пользователя
+        'HOST': 'localhost',  # хост сервера БД
+        'PORT': '5432',       # порт PostgreSQL
+    }
+}
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+_DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',  # движок для PostgreSQL
+        'NAME': 'afina',       # имя базы данных
+        'USER': 'postgres',   # имя пользователя PostgreSQL
+        'PASSWORD': '123',  # пароль пользователя
+        'HOST': 'localhost',  # хост сервера БД
+        'PORT': '5432',       # порт PostgreSQL
+    }
+}
+
+__DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',  # движок для PostgreSQL
+        'NAME': 'afina',       # имя базы данных
+        'USER': 'postgres',   # имя пользователя PostgreSQL
+        'PASSWORD': '123',  # пароль пользователя
+        'HOST': 'localhost',  # хост сервера БД
+        'PORT': '5432',       # порт PostgreSQL
+    }
+}
+
+___DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',  # движок для PostgreSQL
+        'NAME': 'afina',       # имя базы данных
+        'USER': 'postgres',   # имя пользователя PostgreSQL
+        'PASSWORD': '123',  # пароль пользователя
+        'HOST': 'localhost',  # хост сервера БД
+        'PORT': '5432',       # порт PostgreSQL
+    }
+}
+
+____DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',  # движок для PostgreSQL
+        'NAME': 'afina',       # имя базы данных
+        'USER': 'postgres',   # имя пользователя PostgreSQL
+        'PASSWORD': '123',  # пароль пользователя
+        'HOST': 'localhost',  # хост сервера БД
+        'PORT': '5432',       # порт PostgreSQL
+    }
+}
